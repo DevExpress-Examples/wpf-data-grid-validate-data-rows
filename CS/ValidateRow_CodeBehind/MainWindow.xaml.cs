5f18acf3c8ee86e35efdf61b6506a81f06ae42de
aliev.faramoz <faramoz.aliev@devexpress.com>
2021-09-22 12:35:47 +0300

    update

--- CS/ValidateRow_CodeBehind/MainWindow.xaml.cs
+++ CS/ValidateRow_CodeBehind/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Windows;
 
 namespace ValidateRow_CodeBehind {
@@ -12,20 +11,19 @@ namespace ValidateRow_CodeBehind {
         public DateTime EndDate { get; set; }
 
         string IDataErrorInfo.Error => StartDate > EndDate ? "Either Start Date or End Date should be corrected." : null;
-
         string IDataErrorInfo.this[string columnName] {
             get {
                 switch(columnName) {
                     case nameof(StartDate):
                         if(StartDate > EndDate)
-                           return "Start Date must be less than End Date";
+                            return "Start Date must be less than End Date";
                         break;
                     case nameof(EndDate):
                         if(StartDate > EndDate)
                             return "End Date must be greater than Start Date";
                         break;
                     case nameof(TaskName):
-                        if(string.IsNullOrEmpty(TaskName) || string.IsNullOrWhiteSpace(TaskName))
+                        if(string.IsNullOrEmpty(TaskName))
                             return "Enter a task name";
                         break;
                 }
@@ -36,20 +34,22 @@ namespace ValidateRow_CodeBehind {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            grid.ItemsSource = GetData().ToList();
-        }
-        static IEnumerable<Task> GetData() {
-            yield return new Task() {
-                TaskName = "Complete Project A",
-                StartDate = new DateTime(2009, 7, 17), EndDate = new DateTime(2009, 7, 10)
-            };
-            yield return new Task() {
-                TaskName = "Test Website",
-                StartDate = new DateTime(2009, 7, 10), EndDate = new DateTime(2009, 7, 12)
-            };
-            yield return new Task() {
-                TaskName = "",
-                StartDate = new DateTime(2009, 7, 4), EndDate = new DateTime(2009, 7, 6)
+            grid.ItemsSource = new List<Task> {
+                new Task() {
+                    TaskName = "Complete Project A",
+                    StartDate = new DateTime(2009, 7, 17),
+                    EndDate = new DateTime(2009, 7, 10)
+                },
+                new Task() {
+                    TaskName = "Test Website",
+                    StartDate = new DateTime(2009, 7, 10),
+                    EndDate = new DateTime(2009, 7, 12)
+                },
+                new Task() {
+                    TaskName = string.Empty,
+                    StartDate = new DateTime(2009, 7, 4),
+                    EndDate = new DateTime(2009, 7, 6)
+                }
             };
         }
 
@@ -57,7 +57,7 @@ namespace ValidateRow_CodeBehind {
             var task = (Task)e.Row;
             if(task.StartDate > task.EndDate)
                 e.SetError("Start Date must be less than End Date");
-            if(string.IsNullOrEmpty(task.TaskName) || string.IsNullOrWhiteSpace(task.TaskName))
+            if(string.IsNullOrEmpty(task.TaskName))
                 e.SetError("Enter a task name");
         }
 
--- CS/ValidateRow_MVVM/MainViewModel.cs
+++ CS/ValidateRow_MVVM/MainViewModel.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using DevExpress.Mvvm;
+﻿using DevExpress.Mvvm;
 using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm.Xpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ValidateRow_MVVM {
     public class Task : IDataErrorInfo {
@@ -13,7 +12,6 @@ namespace ValidateRow_MVVM {
         public DateTime EndDate { get; set; }
 
         string IDataErrorInfo.Error => StartDate > EndDate ? "Either Start Date or End Date should be corrected." : null;
-
         string IDataErrorInfo.this[string columnName] {
             get {
                 switch(columnName) {
@@ -26,7 +24,7 @@ namespace ValidateRow_MVVM {
                             return "End Date must be greater than Start Date";
                         break;
                     case nameof(TaskName):
-                        if(string.IsNullOrEmpty(TaskName) || string.IsNullOrWhiteSpace(TaskName))
+                        if(string.IsNullOrEmpty(TaskName))
                             return "Enter a task name";
                         break;
                 }
@@ -36,22 +34,24 @@ namespace ValidateRow_MVVM {
     }
 
     public class MainViewModel : ViewModelBase {
-        public ObservableCollection<Task> TaskList { get; }
+        public List<Task> TaskList { get; }
         public MainViewModel() {
-            TaskList = new ObservableCollection<Task>(GetData());
-        }
-        static IEnumerable<Task> GetData() {
-            yield return new Task() {
-                TaskName = "Complete Project A",
-                StartDate = new DateTime(2009, 7, 17), EndDate = new DateTime(2009, 7, 10)
-            };
-            yield return new Task() {
-                TaskName = "Test Website",
-                StartDate = new DateTime(2009, 7, 10), EndDate = new DateTime(2009, 7, 12)
-            };
-            yield return new Task() {
-                TaskName = "",
-                StartDate = new DateTime(2009, 7, 4), EndDate = new DateTime(2009, 7, 6)
+            TaskList = new List<Task> {
+                new Task() {
+                    TaskName = "Complete Project A",
+                    StartDate = new DateTime(2009, 7, 17),
+                    EndDate = new DateTime(2009, 7, 10)
+                },
+                new Task() {
+                    TaskName = "Test Website",
+                    StartDate = new DateTime(2009, 7, 10),
+                    EndDate = new DateTime(2009, 7, 12)
+                },
+                new Task() {
+                    TaskName = string.Empty,
+                    StartDate = new DateTime(2009, 7, 4),
+                    EndDate = new DateTime(2009, 7, 6)
+                }
             };
         }
 
@@ -62,7 +62,7 @@ namespace ValidateRow_MVVM {
         static ValidationErrorInfo GetValidationErrorInfo(Task task) {
             if(task.StartDate > task.EndDate)
                 return new ValidationErrorInfo("Start Date must be less than End Date");
-            if(string.IsNullOrEmpty(task.TaskName) || string.IsNullOrWhiteSpace(task.TaskName))
+            if(string.IsNullOrEmpty(task.TaskName))
                 return new ValidationErrorInfo("Enter a task name");
             return null;
         }
